3584ff77f903fe1a1f92cc0751bd93636cc8711b
hungmegame123fpt <hungthinh26072004@gmail.com>
2025-07-14 01:35:16 +0700

    .

---
+++ BussinessObject/DTO/SuggestedJob.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BusinessObject.DTO
+{
+    public class SuggestedJob
+    {
+        [JsonPropertyName("job_title")]
+        public string JobTitle { get; set; } = null!;
+
+        [JsonPropertyName("company_name")]
+        public string CompanyName { get; set; } = null!;
+
+        [JsonPropertyName("required_skills")]
+        public List<string> RequiredSkills { get; set; } = new();
+
+        [JsonPropertyName("salary_range")]
+        public string SalaryRange { get; set; } = null!;
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; } = null!;
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; } = null!;
+    }
+}
